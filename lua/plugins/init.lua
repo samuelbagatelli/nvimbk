1c366501701b1170abdfd0b110f0d78ff1a136aa
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2025-07-29 08:25:24 -0300

    feat: adding rust support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,7 +11,6 @@ return {
       require "configs.lspconfig"
     end,
   },
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -30,4 +29,73 @@ return {
       },
     },
   },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = {
+            type = "executable",
+            command = "codelldb",
+            args = { "--port", "${port}" },
+          },
+          configurations = {
+            {
+              type = "codelldb",
+              request = "launch",
+              name = "Launch file",
+              program = function()
+                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = "${workspaceFolder}",
+              stopOnEntry = true,
+            },
+          },
+        },
+        tools = {
+          inlay_hints = {
+            auto = true,
+            show_parameter_names = true,
+            parameter_names_prefix = "",
+            type_hints_prefix = ":",
+            other_hints_prefix = "=>",
+          },
+        },
+      }
+    end,
+  },
+
+  { "rust-lang/rust.vim" },
+
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    config = function()
+      require("crates").setup()
+    end,
+  },
+
+  -- Para autocomplicação (se você ainda não tem um)
+  -- Exemplo com nvim-cmp:
+  -- {
+  --   'hrsh7th/nvim-cmp',
+  --   dependencies = {
+  --     'hrsh7th/cmp-nvim-lsp',
+  --     'L3MON4D3/LuaSnip', -- ou outro motor de snippet
+  --     'saecki/crates.nvim', -- Integração com crates.nvim
+  --   },
+  --   config = function()
+  --     local cmp = require('cmp')
+  --     cmp.setup({
+  --       sources = cmp.config.sources({
+  --         { name = 'nvim_lsp' },
+  --         { name = 'luasnip' },
+  --         { name = 'crates' }, -- Adiciona a fonte para crates
+  --       }),
+  --       -- Outras configurações cmp...
+  --     })
+  --   end,
+  -- },
 }

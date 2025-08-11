bf6da617a2b32b0ebdbf8c54d8175fe088a8df05
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2025-08-11 08:44:24 -0300

    feat: vue definitive suppert

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,8 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers =
-  { "html", "cssls", "pyright", "clangd", "rust_analyzer", "vue-language-server", "typescript-language-server" }
+local servers = { "html", "cssls", "pyright", "clangd", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -25,3 +24,48 @@ lspconfig.clangd.setup {
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
 }
+
+-- Configuração do TypeScript Language Server
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    typescript = {
+      preferences = {
+        disableSuggestions = false,
+      },
+    },
+    javascript = {
+      preferences = {
+        disableSuggestions = false,
+      },
+    },
+  },
+}
+
+-- Configuração do Volar (Vue Language Server)
+lspconfig.volar.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+  init_options = {
+    typescript = {
+      tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+    },
+  },
+  settings = {
+    volar = {
+      completion = {
+        autoImportComponent = true,
+      },
+      codeLens = {
+        references = true,
+        pugTools = true,
+        scriptSetupTools = true,
+      },
+    },
+  },
+}
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
       require "configs.lspconfig"
     end,
   },
+
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -25,12 +26,26 @@ return {
         "c",
         "cpp",
         "javascript",
-        "toml",
-        "vue",
         "typescript",
+        "tsx",
+        "vue",
+        "json",
+        "yaml",
+        "toml",
       },
     },
   },
+
+  {
+    "posva/vim-vue",
+    ft = { "vue" },
+  },
+
+  {
+    "leafgarland/typescript-vim",
+    ft = { "typescript", "typescriptreact" },
+  },
+
   {
     "mrcjkb/rustaceanvim",
     version = "^3",
@@ -80,25 +95,62 @@ return {
     end,
   },
 
-  -- Para autocomplicação (se você ainda não tem um)
-  -- Exemplo com nvim-cmp:
   -- {
-  --   'hrsh7th/nvim-cmp',
+  --   "hrsh7th/nvim-cmp",
   --   dependencies = {
-  --     'hrsh7th/cmp-nvim-lsp',
-  --     'L3MON4D3/LuaSnip', -- ou outro motor de snippet
-  --     'saecki/crates.nvim', -- Integração com crates.nvim
+  --     "hrsh7th/cmp-nvim-lsp",
+  --     "hrsh7th/cmp-buffer",
+  --     "hrsh7th/cmp-path",
+  --     "L3MON4D3/LuaSnip",
+  --     "saednix/cmp-luasnip",
+  --     "saecki/crates.nvim",
   --   },
   --   config = function()
-  --     local cmp = require('cmp')
-  --     cmp.setup({
+  --     local cmp = require "cmp"
+  --     local luasnip = require "luasnip"
+  --
+  --     cmp.setup {
+  --       snippet = {
+  --         expand = function(args)
+  --           luasnip.lsp_expand(args.body)
+  --         end,
+  --       },
+  --       mapping = cmp.mapping.preset.insert {
+  --         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+  --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --         ["<C-Space>"] = cmp.mapping.complete(),
+  --         ["<CR>"] = cmp.mapping.confirm {
+  --           behavior = cmp.ConfirmBehavior.Replace,
+  --           select = true,
+  --         },
+  --         ["<Tab>"] = cmp.mapping(function(fallback)
+  --           if cmp.visible() then
+  --             cmp.select_next_item()
+  --           elseif luasnip.expand_or_jumpable() then
+  --             luasnip.expand_or_jump()
+  --           else
+  --             fallback()
+  --           end
+  --         end, { "i", "s" }),
+  --         ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --           if cmp.visible() then
+  --             cmp.select_prev_item()
+  --           elseif luasnip.jumpable(-1) then
+  --             luasnip.jump(-1)
+  --           else
+  --             fallback()
+  --           end
+  --         end, { "i", "s" }),
+  --       },
   --       sources = cmp.config.sources({
-  --         { name = 'nvim_lsp' },
-  --         { name = 'luasnip' },
-  --         { name = 'crates' }, -- Adiciona a fonte para crates
+  --         { name = "nvim_lsp" },
+  --         { name = "luasnip" },
+  --         { name = "crates" },
+  --       }, {
+  --         { name = "buffer" },
+  --         { name = "path" },
   --       }),
-  --       -- Outras configurações cmp...
-  --     })
+  --     }
   --   end,
   -- },
 }
